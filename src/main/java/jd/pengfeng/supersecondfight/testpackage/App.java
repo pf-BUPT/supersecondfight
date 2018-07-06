9ad15e455945536df22041bfbcba14b84aa64bc0
pf-BUPT <849781191@qq.com>
2018-07-06 14:08:32 +0800

    其他人修改了App

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -7,6 +7,7 @@ public class App {
 		System.out.println("我在本地修改了App.java");
 		System.out.println("我在本地新加了一行，我是pengfeng");
 		System.out.println("其他人删除了helloe jd，并且增加了一行");
+		System.out.println("我在本地又新加了一行，我是软开工程师！");
 	}
 
 }
