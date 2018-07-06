8fd500deb10574fa7b299b7b78d5cfc82f847b2e
pf-BUPT <849781191@qq.com>
2018-07-06 14:28:41 +0800

    有人删除了pengfeng，还增加了一行

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -5,9 +5,9 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("我在本地修改了App.java");
-		System.out.println("我在本地新加了一行，我是pengfeng");
 		System.out.println("其他人删除了helloe jd，并且增加了一行");
 		System.out.println("我在本地又新加了一行，我是软开工程师！");
+		System.out.println("有人又增加了一行，并删除了pengfeng");
 	}
 
 }
