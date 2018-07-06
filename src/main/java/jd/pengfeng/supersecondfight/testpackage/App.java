9a65e419ffcbd5f0c1bdad150325e368120ac312
pengfeng21 <pengfeng3@jd.com>
2018-07-06 11:40:02 +0800

    提交！

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -5,8 +5,10 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("其他人删除了Hello world");
-		System.out.println("其他人修改了这个文件！");
+
+		System.out.println("Hello jd");
+		System.out.println("我在本地修改了App.java");
+		System.out.println("我在本地新加了一行，我是pengfeng");
 	}
 
 }
