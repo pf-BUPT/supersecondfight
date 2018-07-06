281eceb58002adff920469ef4b2e76f6ffba50e9
pengfeng21 <pengfeng3@jd.com>
2018-07-06 14:29:47 +0800

    pengfeng修改14:29

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -8,6 +8,8 @@ public class App {
 		System.out.println("其他人删除了helloe jd，并且增加了一行");
 		System.out.println("我在本地又新加了一行，我是软开工程师！");
 		System.out.println("有人又增加了一行，并删除了pengfeng");
+		System.out.println("我在本地又新加了一行，我是软开工程师pengfeng！");
+
 	}
 
 }
