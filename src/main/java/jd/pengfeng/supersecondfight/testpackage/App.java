6a49130c91562a0fb07d3702bddb4f22ea0d14e6
pf-BUPT <849781191@qq.com>
2018-07-06 14:08:32 +0800

    其他人修改了App

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -4,11 +4,9 @@ package jd.pengfeng.supersecondfight.testpackage;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hello jd");
 		System.out.println("我在本地修改了App.java");
 		System.out.println("我在本地新加了一行，我是pengfeng");
+		System.out.println("其他人删除了helloe jd，并且增加了一行");
 	}
 
 }
