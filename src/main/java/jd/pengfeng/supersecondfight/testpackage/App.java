27b0ba12c75d4bf704d37e1c040cea00f1b264fb
pf-BUPT <849781191@qq.com>
2018-07-06 11:07:53 +0800

    其他人修改了App.java

--- src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
+++ src/main/java/jd/pengfeng/supersecondfight/testpackage/App.java
@@ -5,7 +5,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello jd");
+		System.out.println("其他人删除了Hello world");
 		System.out.println("其他人修改了这个文件！");
 	}
 
